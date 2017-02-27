aa2d6deb12148466f963b4373aca5d3b65a0a9b8
Rajashree Malvade <rajashreermalvade@gmail.com>
2017-02-27 13:23:52 +0530

    Add view pager that gets fragements from custom pager adapter. First cut

---
+++ app/src/main/java/com/example/android/viewpagerapplication/CustomFragmentPagerAdapter.java
@@ -0,0 +1,22 @@
+package com.example.android.viewpagerapplication;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class CustomFragmentPagerAdapter extends FragmentPagerAdapter{
+
+    public CustomFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
