4ddf892c1a8a02f88d70654f4af790edd4fff694
Rajashree Malvade <rajashreermalvade@gmail.com>
2017-02-27 13:31:38 +0530

    Set text in fragment programmatically

--- app/src/main/java/com/example/android/viewpagerapplication/DayFragment.java
+++ app/src/main/java/com/example/android/viewpagerapplication/DayFragment.java
@@ -6,20 +6,23 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class DayFragment extends Fragment {
-
-
     public DayFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_day, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_day, container, false);
+
+        TextView day = (TextView) rootView.findViewById(R.id.day);
+        day.setText("Which day is it today?");
+
+        return rootView;
     }
 
 }
